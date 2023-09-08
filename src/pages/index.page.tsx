7b70015a4eb34a389aeb67187f4997a31b39d7bc
Matheus Baron <matheusbaron10@gmail.com>
2023-09-08 11:12:30 -0300

    chore(next): update Next page extensions configuration

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,5 @@
 import { Heading } from '@ignite-ui/react'
 
 export default function Home() {
-  return (
-    <Heading as='h1' >Hello Ignite Call</Heading>
-  )
+  return <Heading as="h1">Hello Ignite Call</Heading>
 }
