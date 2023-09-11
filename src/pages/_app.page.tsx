264be1504938ed540b9e30f4e9466aff99d6e1d9
Matheus Baron <matheusbaron10@gmail.com>
2023-09-11 12:40:22 -0300

    feat(register): add next-auth to connect with user google account

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+import { SessionProvider } from 'next-auth/react'
 
 globalStyles()
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -3,10 +3,9 @@ import { Container, Header } from '../styles'
 import { ArrowRight } from 'phosphor-react'
 // import { api } from '../../lib/axios'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn } from 'next-auth/react'
 
 export default function Register() {
-  // async function handleRegister(data) { }
-
   return (
     <Container>
       <Header>
@@ -21,7 +20,11 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
